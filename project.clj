2df99f46a12c2e2f2cc3db77d53f35f41c26453a
Jason Lewis <jason@decomplecting.org>
2016-10-15 19:21:22 -0400

    Begin major overhaul of client

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
-(defproject anjel "0.0.1"
+(defproject anjel "0.1.0-SNAPSHOT"
   :description "A Clojure client for the AngelList API"
   :url "https://github.com/canweriotnow/anjel"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clj-http "0.7.7"]
-                 [org.clojure/data.json "0.2.3"]
-                 [lein-kibit "0.0.8"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-http "0.7.7"] ;; Replace w/ http-kit
+                 [http-kit "2.1.18"] ;; Replace calls to clj-http
+                 [org.clojure/data.json "0.2.3"] ;; Replace w/ cheshire
+                 [cheshire "5.6.3"] ;; Replacing data.json
+                 ])
